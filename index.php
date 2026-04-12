b22239729487098428e37003507083d6c0a16e4a
devphpru <wixphp@gmail.com>
2026-04-12 14:59:28 +0300

    Внес правки в проект

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-declare(strict_types=1);
-error_reporting(-1);
-session_start();
-
---
+++ tests/Example.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests;
+
+class Example
+{
+    public function response(): bool
+    {
+        return true;
+    }
+
+}
